d20f840006490ba59f58f120b0638a36d57f96be
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-11-12 14:32:53 +0300

    Add files via upload

---
+++ Ind1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Ind1Lab3
+{
+    class Program
+    {       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Матрица:");
+            int[,] array = new int[9, 9];
+            int i, j;
+            Random rand = new Random();
+            for (i = 0; i < 9; i++)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(new String('-', 45));
+                for (j = 0; j < 9; j++)
+                {
+                    array[i, j] = rand.Next(-10,10);
+                    Console.Write($"|{array[i,j],3}|");
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine(new String('-', 45));
+            Console.WriteLine("Матрица зеркальная относительно главной диагонали:");
+            for (i = 0; i < 9; i++)
+            {
+                Console.WriteLine(" ");
+                Console.WriteLine(new String('-', 45));
+                for (j = 0; j < 9; j++)
+                {
+                    Console.Write($"|{array[j, i],3}|");
+                }
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine(new String('-', 45));
+        }
+    }
+}
