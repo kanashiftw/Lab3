52c8fb206ceb7bc6d9d0d012a89ebeeba0e10293
kanashiftw <92251137+kanashiftw@users.noreply.github.com>
2021-11-13 15:56:28 +0300

    Add files via upload

---
+++ 8.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Zadanie8Lab3
+{
+    class Program
+    {      
+        static int[,] Minor(int[,] myArray, int n)
+        {
+            int [,] result = new int[myArray.GetLength(0) - 1, myArray.GetLength(0) - 1];
+            for (int i = 1; i < myArray.GetLength(0); i++)
+            {
+                for (int j = 0, col = 0; j < myArray.GetLength(1); j++)
+                {
+                    if (j == n) continue;
+                    result[i - 1, col] = myArray[i, j];
+                    col++;
+                }
+            }
+            return result;
+        }
+        static double DetMatrix(int[,] myArray)
+        {
+            if (myArray.Length == 1) return myArray[0, 0];
+            if (myArray.Length == 4) return myArray[0, 0] * myArray[1, 1] - myArray[1, 0] * myArray[0, 1];
+            else
+            {
+                double result = 0;
+                for (int i = 0; i < myArray.GetLength(0); i++)
+                {
+                    int[,] Mnr = Minor(myArray, i);
+                    result += Math.Pow(-1, i) * myArray[0, i] * DetMatrix(Mnr);
+                }
+                return result;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int i, j;
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите размерность матрицы.");
+                    i = int.Parse(Console.ReadLine());
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Такое число нельзя обработать");
+                    continue;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Вы ввели не число.");
+                    continue;
+                } break;
+            }
+            j = i;
+            int[,]array = new int[i,j];
+            Console.WriteLine(array.GetLength(0));
+            Random rand = new Random();
+            for (i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine("");
+                Console.WriteLine(new string('-', array.GetLength(0) * 5));
+                for(j = 0; j < array.GetLength(0); j++)
+                {
+                    array[i, j] = rand.Next(-10,10);
+                    Console.Write($"|{array[i, j], 3}|");
+                }
+            }
+            Console.WriteLine("");
+            Console.WriteLine(new string('-', array.GetLength(0) * 5));
+            Console.WriteLine("");
+            Console.WriteLine("Определитель матрицы: " + DetMatrix(array));
+        }
+    }
+}
